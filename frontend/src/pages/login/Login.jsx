05b47e064699ec28e9a807392ecff3b777ef0bc2
Juan Camilo Santa Gomez <santa51107@gmail.com>
2023-05-03 14:41:56 -0500

    Cambio de idiomas en Login y Registro

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,19 +1,22 @@
 import React, { Suspense } from "react";
+import { useTranslation } from "react-i18next";
 //import { useFetch } from "../../fetch/useFetch";
 import { useState } from "react";
 import logo from "../../assets/Logo_Vertical_Azul.png";
 import "./login.scss";
 import icon from "../../assets/flecha.png";
 import { useNavigate } from "react-router-dom";
+import LanguageIcon from "@mui/icons-material/Language";
 
 const Login = () => {
+  const [t, i18n] = useTranslation("login");
   const [correo, setCorreo] = useState("");
   const [contrasena, setContrasena] = useState("");
   //const [data, setData] = useState(null);
   const navigate = useNavigate();
 
   const consultaUsuarioBD = async (datos) => {
-    const data = await fetch('http://127.0.0.1:8000/core/login',datos);
+    const data = await fetch("http://127.0.0.1:8000/core/login", datos);
     return data.json();
   };
 
@@ -33,71 +36,90 @@ const Login = () => {
     console.log("los Datos:", datos);
 
     const response = await consultaUsuarioBD(datos);
-    console.log("response:",response)
-    navigate("/home")
-  }
+    console.log("response:", response);
+    navigate("/home");
+  };
 
   return (
-    <div className="Login grid grid-cols-1 lg:grid-cols-2 min-h-screen">
-      <div
-        className="inicio"
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        <img src={icon} alt="regresar" />
-        <p>Inicio</p>
-      </div>
-      <div className="flex flex-col items-center justify-center bg-blue-950 rounded-tl-lg rounded-bl-lg p-4">
-        <div className="flex flex-col items-center gap-8">
-          <h1 className="text-4xl font-bold text-gray-50">Bienvenido</h1>
+    <>
+      <div className="Login grid grid-cols-1 lg:grid-cols-2 min-h-screen">
+        <div className="idioma">
+          <LanguageIcon
+            style={{ color: "black", fontSize: 40, cursor: "pointer" }}
+            onClick={() => {
+              i18n.language === "es"
+                ? i18n.changeLanguage("en")
+                : i18n.changeLanguage("es");
+            }}
+          />
         </div>
-        <div className="my-14">
-          <p className="text-center relative text-gray-50 before:max-w-[50px] md:before:max-w-[120px] before:w-full before:-left-[60px] md:before:-left-[140px] before:h-[1px] before:bg-current before:absolute before:top-[50%] after:max-w-[50px] md:after:max-w-[120px] after:w-full after:h-[1px] after:bg-current after:absolute after:top-[50%] after:-right-[60px] md:after:-right-[140px]">
-            Ingresa con tu email
-          </p>
+        <div
+          className="inicio"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <img src={icon} alt="regresar" />
+          <p>{t("home")}</p>
         </div>
-        <div className="w-full mb-8">
-          <form>
-            <div className="flex justify-center mb-4">
-              <input
-                type="email"
-                className="w-full max-w-md py-2 px-4 rounded-lg outline-none"
-                placeholder="Correo electrónico"
-                onChange={(e) => setCorreo(e.target.value)}
-              />
-            </div>
-            <div className="flex justify-center mb-6">
-              <input
-                type="password"
-                className="w-full max-w-md py-2 px-4 rounded-lg outline-none"
-                placeholder="Password"
-                onChange={(e) => setContrasena(e.target.value)}
-              />
-            </div>
-            <div className="w-full max-w-md mx-auto flex items-center justify-between text-gray-500 mb-8"></div>
-            <div className="w-full max-w-md mx-auto">
-              <button
-                type="submit"
-                className="w-full bg-gray-200 py-2 px-4 rounded-lg text-gray-900 hover:bg-gray-300 transition-colors"
-                onClick={login}
+        <div className="flex flex-col items-center justify-center bg-blue-950 p-4">
+          <div className="flex flex-col items-center gap-8">
+            <h1 className="text-4xl font-bold text-gray-50">{t("welcome")}</h1>
+          </div>
+          <div className="my-14">
+            <p className="text-center relative text-gray-50 before:max-w-[50px] md:before:max-w-[120px] before:w-full before:-left-[60px] md:before:-left-[140px] before:h-[1px] before:bg-current before:absolute before:top-[50%] after:max-w-[50px] md:after:max-w-[120px] after:w-full after:h-[1px] after:bg-current after:absolute after:top-[50%] after:-right-[60px] md:after:-right-[140px]">
+              {t("sub_text")}
+            </p>
+          </div>
+          <div className="w-full mb-8">
+            <form>
+              <div className="flex justify-center mb-4">
+                <input
+                  type="email"
+                  className="w-full max-w-md py-2 px-4 rounded-lg outline-none"
+                  placeholder={t("email")}
+                  onChange={(e) => setCorreo(e.target.value)}
+                />
+              </div>
+              <div className="flex justify-center mb-6">
+                <input
+                  type="password"
+                  className="w-full max-w-md py-2 px-4 rounded-lg outline-none"
+                  placeholder={t("password")}
+                  onChange={(e) => setContrasena(e.target.value)}
+                />
+              </div>
+              <div className="w-full max-w-md mx-auto flex items-center justify-between text-gray-500 mb-8"></div>
+              <div className="w-full max-w-md mx-auto">
+                <button
+                  type="submit"
+                  className="w-full bg-gray-200 py-2 px-4 rounded-lg text-gray-900 hover:bg-gray-300 transition-colors"
+                  onClick={login}
+                >
+                  {t("login")}
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="cuenta">
+            <span className="text-gray-50">
+              {t("question")}{" "}
+              <p
+                className="registrarme"
+                onClick={() => {
+                  navigate("/register");
+                }}
               >
-                Iniciar sesión
-              </button>
-            </div>
-          </form>
+                {t("register")}
+              </p>
+            </span>
+          </div>
         </div>
-        <div className="cuenta">
-          <span className="text-gray-50">
-            ¿No tienes cuenta?{" "}
-            <p className="registrarme" onClick={() => {navigate("/register")}}>Registrate</p>
-          </span>
+        <div className="hidden lg:flex items-center justify-center border-t border-r border-b">
+          <img src={logo} />
         </div>
       </div>
-      <div className="hidden lg:flex items-center justify-center border-t border-r border-b rounded-tr-lg rounded-br-lg">
-        <img src={logo} />
-      </div>
-    </div>
+    </>
   );
 };
 
